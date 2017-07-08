e6e441d08c081032d134669f9a96c8f94f47702a
Jed <johon8@gmail.com>
2017-07-07 20:48:47 -0400

    refactor to meet best practices

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -5,9 +5,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Announcements from "./components/announcementsPanel";
 
-//function component
-const App = () => {
-	let ann = [
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this._handleFilter = this._handleFilter.bind(this);
+        this._createOptions = this._createOptions.bind(this);
+        this.state = {
+            annData: [],
+            department: "All"
+        };
+    }
+    componentDidMount() {
+        this.setState({
+            annData: [
                 {
                     title: "announcement 1",
                     department: "HQ",
@@ -58,12 +68,41 @@ const App = () => {
                     department: "HQ",
                     body: "some event is happening here"
                 },
-            ];
-	return (
-		<div>
-			<Announcements announcements={ann}/>
-		</div>
-	);
-};
+            ]
+        });
+    }
+    _handleFilter(event) {
+        let selected = event.target.value;
+        this.setState({
+            department: selected
+        });
+    }
+    _createOptions() {
+        let selections = this.state.annData.reduce((ary, item) => {
+            if(ary.indexOf(item.department) === -1) {
+                ary.push(item.department);
+            }
+            return ary;
+        }, []);
+        selections.sort();
+        selections.unshift("All");
+        return selections.map((item, i) => {
+            return <option key={i+1} value={item}>{item}</option>;
+        });
+    }
+    render() {
+        return (
+            <div>
+            <Announcements 
+                selectHandler = {this._handleFilter}
+                optionsCreator = {this._createOptions}
+                announcements = {this.state.annData}
+                filtered = {this.state.department !== "All"}
+                filterDepartment = {this.state.department}
+            />
+        </div>
+        );
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- src/scripts/components/announcementsPanel.jsx
+++ src/scripts/components/announcementsPanel.jsx
@@ -8,70 +8,42 @@ import {Announcement} from "./components";
 import propTypes from "prop-types";
 import styles from "../../styleSheets/main";
 
-export default class Announcements extends React.Component {
-    constructor(props) {
-        super(props);
-        this._handleFilter = this._handleFilter.bind(this);
-        this._createOptions = this._createOptions.bind(this);
-        this.state = {
-            annData: [],
-            department: "All"
-        };
-    }
-    componentDidMount() {
-        this.setState({
-            annData: this.props.announcements
-        });
-    }
-    _handleFilter(event) {
-        let selected = event.target.value;
-        this.setState({
-            department: selected
-        });
-    }
-    _createOptions() {
-        let selections = this.state.annData.reduce((ary, item) => {
-            if(ary.indexOf(item.department) === -1) {
-                ary.push(item.department);
-            }
-            return ary;
-        }, []);
-        selections.sort();
-        selections.unshift("All");
-        return selections.map((item, i) => {
-            return <option key={i+1} value={item}>{item}</option>;
-        });
-    }
-    render() {
-        let filtered = this.state.department !== "All";
-        return (
-            <div className="announceContainer">
-                <div className="selectControl">
-                    <div>
-                        <span className="annFilterLabel">See specific organization</span>
-                        <span>
-                            <select className="orgs" onChange={this._handleFilter}>
-                                {this._createOptions()}
-                            </select>
-                        </span>
-                    </div>
-                </div>
-                <div className="announcements">
-                    {
-                        //loop thru annoucements
-                        this.state.annData.reduce((ary, a, i) => {
-
-                            if (!filtered || filtered && a.department === this.state.department) {
-                                ary.push(<Announcement key={i} announce={a}/>);
-                            }
-                            return ary;
-                        },[])
-                    }
+const Announcements = (props) => {
+    
+    return (
+        <div className="announceContainer">
+            <div className="selectControl">
+                <div>
+                    <span className="annFilterLabel">See specific organization</span>
+                    <span>
+                        <select className="orgs" onChange={props.selectHandler}>
+                            {props.optionsCreator()}
+                        </select>
+                    </span>
                 </div>
             </div>
-        );
-    }
-}
-Announcements.propTypes = {
-    announcements: propTypes.array.isRequired
+            <div className="announcements">
+                {
+                    //loop thru annoucements
+                    props.announcements.reduce((ary, a, i) => {
+
+                        if (!props.filtered || props.filtered && a.department === props.filterDepartment) {
+                            ary.push(<Announcement key={i} announce={a}/>);
+                        }
+                        return ary;
+                    },[])
+                }
+            </div>
+        </div>
+        
+    );
 };
+Announcements.propTypes = {
+    selectHandler: propTypes.func.isRequired,
+    optionsCreator: propTypes.func.isRequired,
+    announcements: propTypes.array.isRequired,
+    filtered: propTypes.bool.isRequired,
+    filterDepartment: propTypes.string.isRequired
+};
+
+export default Announcements;
